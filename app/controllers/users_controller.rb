f5492887c37eea06be7514bf7ab773c171ece988
Cameron Cross <camcross94@gmail.com>
2014-07-26 10:03:24 +0200

    edited email feature to be compatible with heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UserController < ApplicationController
+
 	def index
 	end
+
+	def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 end
